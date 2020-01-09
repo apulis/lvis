afa923de3f42245c4c0dff4caaa94fd69b12f8c2
OYT <ytaek-oh>
2020-01-09 19:37:33 +0900

    sigmoid

--- mmdet/models/bbox_heads/bbox_head.py
+++ mmdet/models/bbox_heads/bbox_head.py
@@ -203,7 +203,11 @@ class BBoxHead(nn.Module):
                        cfg=None):
         if isinstance(cls_score, list):
             cls_score = sum(cls_score) / float(len(cls_score))
-        scores = F.softmax(cls_score, dim=1) if cls_score is not None else None
+        if self.use_sigmoid_cls:
+            scores = F.sigmoid(cls_score) if cls_score is not None else None
+        else:
+            scores = F.softmax(
+                cls_score, dim=1) if cls_score is not None else None
         if bbox_pred is not None:
             bboxes = delta2bbox(rois[:, 1:], bbox_pred, self.target_means,
                                 self.target_stds, img_shape)
